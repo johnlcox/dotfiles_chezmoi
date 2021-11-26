17a17450744b5f2a61e382c708a2080873639541
John Leacox <johnlcox@gmail.com>
2021-11-26 13:51:34 -0600

    Add Readline to powershell profile for partial history completion

--- Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,6 @@
 if (Test-Path -Path "$HOME/.jenv") {
     $env:Path += ";$HOME/.jenv"
 }
+
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
