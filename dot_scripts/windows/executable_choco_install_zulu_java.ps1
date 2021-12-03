c0d98d2b0c00ba199873625556f9b99e918df3f2
John Leacox <johnlcox@gmail.com>
2021-12-02 21:19:22 -0600

    Cleanup choco install zulu script

--- dot_scripts/windows/executable_choco_install_zulu_java.ps1
+++ dot_scripts/windows/executable_choco_install_zulu_java.ps1
@@ -3,15 +3,21 @@ $zulu_versions = @(
     "zulu17"
 )
 
-choco install $zulu_versions
+choco upgrade $zulu_versions
 
 if (Test-Path -Path "$HOME/.jenv") {
     $env:Path += ";$HOME/.jenv"
 
-    foreach ( $zulu_version in $zulu_versions ) {
+    # jenv uses Write-Host so redirect that to success to capture the output
+    $existing_jenvs = jenv list 6>&1 | Out-String
+    foreach ($zulu_version in $zulu_versions) {
+        if ($existing_jenvs -like "*${zulu_version}*") {
+            continue
+        }
+
         $jdk_dir = $zulu_version.Insert(4, '-')
         $jdk_path = "C:\Program Files\Zulu\${jdk_dir}"
-        echo $jdk_path
+        # echo $jdk_path
         jenv add $zulu_version $jdk_path
     }
 }
