10c204577afc9f2ae63e7e40e0d60341e6f91bf3
John Leacox <johnlcox@gmail.com>
2021-12-09 22:04:17 -0600

    Get JDK installation script working for mac

--- dot_scripts/mac/executable_brew_install_zulu_java.sh
+++ dot_scripts/mac/executable_brew_install_zulu_java.sh
@@ -3,21 +3,27 @@
 # Tap the zulu casks from https://github.com/mdogan/homebrew-zulu
 brew tap mdogan/zulu
 
-zulu_versions=(
-	zulu-jdk11
-	zulu-jdk17
-)
+# Create an associative array of the common jdk version keys to brew package names
+declare -A zulu_versions
+zulu_versions[jdk11]=zulu-jdk11
+zulu_versions[jdk17]=zulu-jdk17
 
-brew upgrade --cask ${casks[@]}
+versions_to_install=()
+for jdk_version in "$@"
+do
+    versions_to_install+=${zulu_versions[$jdk_version]}
+done
 
+brew install --cask ${versions_to_install[@]}
 
 # add to jenv if present
 if [ -d "$HOME/.jenv/bin" ]; then
   eval "$(jenv init -)"
 
-  for zulu_version in "${zulu_versions[@]}"; do
-    jdk_path="/Library/Java/JavaVirtualMachines/${zulu_version%'-jdk'}.jdk/Contents/Home"
-    echo $jdk_path
-    # jenv add $jdk_path
+  for zulu_version in "${versions_to_install[@]}"; do
+    # Use pattern matching on the zulu_version to get just the number
+    # https://stackoverflow.com/a/16623897
+    jdk_path="/Library/Java/JavaVirtualMachines/zulu-${zulu_version#'zulu-jdk'}.jdk/Contents/Home"
+    jenv add $jdk_path
   done
 fi
