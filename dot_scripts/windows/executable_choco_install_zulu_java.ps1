48ed376c2e5930acd7cce0aba7d8103a07b569d1
John Leacox <johnlcox@gmail.com>
2021-12-11 10:50:47 -0600

    Update Windows Java install script to use common jdk names

--- dot_scripts/windows/executable_choco_install_zulu_java.ps1
+++ dot_scripts/windows/executable_choco_install_zulu_java.ps1
@@ -1,16 +1,24 @@
-$zulu_versions = @(
-    "zulu11"
-    "zulu17"
-)
+# Create an associative array of the common jdk version keys to brew package names
+$zulu_versions = @{
+    jdk11="zulu11";
+    jdk17="zulu17"
+}
 
-choco upgrade -y $zulu_versions
+
+$versions_to_install=@()
+foreach ($jdk_version in $args) {
+    $versions_to_install += $zulu_versions[$jdk_version]
+}
+
+
+choco upgrade -y $versions_to_install
 
 if (Test-Path -Path "$HOME\.jenv") {
     $env:Path += ";$HOME\.jenv"
 
     # jenv uses Write-Host so redirect that to success to capture the output
     $existing_jenvs = jenv list 6>&1 | Out-String
-    foreach ($zulu_version in $zulu_versions) {
+    foreach ($zulu_version in $versions_to_install) {
         if ($existing_jenvs -like "*${zulu_version}*") {
             continue
         }
