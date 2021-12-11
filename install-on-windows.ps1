289a49775d8872e13cfc34822c75be4cfd38cfc4
John Leacox <johnlcox@gmail.com>
2021-12-11 11:48:51 -0600

    Update install-on-windows script to init from repo

--- install-on-windows.ps1
+++ install-on-windows.ps1
@@ -1,6 +1,9 @@
 # Install in ./bin
 (iwr -UseBasicParsing https://git.io/chezmoi.ps1).Content | powershell -c -
 
+~/bin/chezmoi.exe init https://github.com/johnlcox/dotfiles_chezmoi.git
+~/bin/chezmoi.exe apply
+
 # TODO: Might be able to automate if this whole script is run with admin privs
 # Enable running powershell scripts (manual step):
 # Set-ExecutionPolicy AllSigned
