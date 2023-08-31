230f8bb42527bacbe32d0d1eceded470dbf68140
John Leacox <johnlcox@gmail.com>
2023-08-31 07:42:38 -0500

    Update oh-my-posh instal path

--- Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -9,6 +9,9 @@ if (Test-Path -Path "$HOME\.jenv") {
 Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
 
-if (Test-Path -Path "~\AppData\Local\Programs\oh-my-posh\themes") {
+if (Test-Path -Path "C:\Program Files (x86)\oh-my-posh\themes") {
+    oh-my-posh --init --shell pwsh --config ~\AppData\Local\Programs\oh-my-posh\themes\hotstick.minimal.omp.json | Invoke-Expression
+}
+elseif (Test-Path -Path "~\AppData\Local\Programs\oh-my-posh\themes") {
     oh-my-posh --init --shell pwsh --config ~\AppData\Local\Programs\oh-my-posh\themes\hotstick.minimal.omp.json | Invoke-Expression
 }
