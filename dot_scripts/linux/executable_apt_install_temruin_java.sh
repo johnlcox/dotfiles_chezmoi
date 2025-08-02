735049d2c58d3c6dd4e99a36bd15f8762729ae90
John Leacox <johnlcox@gmail.com>
2025-08-02 15:31:38 -0500

    Add temurin Java for linux

---
+++ dot_scripts/linux/executable_apt_install_temruin_java.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env zsh
+
+# install Temurin Java on Debian/Ubuntu
+# Instructions: https://adoptium.net/installation/linux#_deb_installation_on_debian_or_ubuntu
+
+# install the necessary dependencies
+apt install -y wget apt-transport-https gpg
+
+# add Eclipse Adoptium GPG key
+wget -qO - https://packages.adoptium.net/artifactory/api/gpg/key/public | gpg --dearmor | tee /etc/apt/trusted.gpg.d/adoptium.gpg > /dev/null
+
+
+
+# download and install the package that adds 
+# the Azul APT repository to the list of sources 
+curl -O https://cdn.azul.com/zulu/bin/zulu-repo_1.0.0-3_all.deb
+
+# configure the Eclipse Adoptium apt repository
+echo "deb https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | tee /etc/apt/sources.list.d/adoptium.list
+
+# update the package sources
+sudo apt update
+
+# Create an associative array of the common jdk version keys to brew package names
+declare -A temurin_versions
+temurin_versions[jdk11]=temurin-11-jdk
+temurin_versions[jdk17]=temurin-17-jdk
+temurin_versions[jdk21]=temurin-21-jdk
+
+versions_to_install=()
+for jdk_version in "$@"
+do
+    versions_to_install+=${temurin_versions[$jdk_version]}
+done
+
+# install Azul Zulu JDKs
+sudo apt install -y ${versions_to_install[@]}
+
+# add to jenv if present
+if [ -d "$HOME/.jenv/bin" ]; then
+  eval "$(jenv init -)"
+
+  for version in "${versions_to_install[@]}"; do
+    jdk_path="/usr/lib/jvm/${version%'-jdk'}"
+    echo $jdk_path
+    jenv add $jdk_path
+  done
+fi
